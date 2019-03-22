d2f07a8817a4f9af8f36108a06d4a685c96ab01d
aerain <j40f893@gmail.com>
2019-03-22 08:26:42 +0000

    1차 테스트 통과

---
+++ userdao/src/UserDao.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class UserDao {
+    public User get(Long id) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection =
+                DriverManager.getConnection("jdbc:mysql://172.18.102.128/jeju?serverTimezone=UTC", "portal", "portaljejunu");
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");
+
+        preparedStatement.setLong(1, id);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        User user = new User();
+        user.setId(resultSet.getLong("id"));
+        user.setName(resultSet.getString("name"));
+        user.setPassword((resultSet.getString("password")));
+
+        return user;
+    }
+}
