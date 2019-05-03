db390c7a7079d9faa78bd9a618192baed87b29a7
aerain <j40f893@gmail.com>
2019-05-03 18:39:02 +0900

    servlet

--- web2/src/main/java/kr/ac/jejunu/servlet/HelloServlet.java
+++ web2/src/main/java/kr/ac/jejunu/servlet/HelloServlet.java
@@ -1,4 +1,30 @@
 package kr.ac.jejunu.servlet;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+
+@Slf4j
 public class HelloServlet extends GenericServlet {
+    @Override
+    public void init() throws ServletException {
+        log.info("servlet init");
+        super.init();
+    }
+
+    @Override
+    public void destroy() {
+        log.info("servlet destroy");
+        super.destroy();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        log.info("servlet service");
+        res.getWriter().println("<h1>hi?</h1>");
+    }
 }
